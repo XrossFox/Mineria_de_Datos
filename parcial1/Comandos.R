7286ead76abe9436f11066c43685a216bce2e9d5
XrossFox <warhead_1090@hotmail.com>
2017-09-08 14:08:45 -0500

    Agregado..
    introduccion, problematica, resumen de variables, resumen estadistico simple. Y Correlacion con scatterplots

--- parcial1/Comandos.R
+++ parcial1/Comandos.R
@@ -4,11 +4,14 @@ cpuData <- read.csv("machine.csv")
 cpuMatrix <- cpuData[-1]
 cpuMatrix <- cpuMatrix[-1]
 
+#Correlacion de la matriz
+
+cor(cpuMatrix)
 #Grafica de scatter `para la correlacion de PRP y MMAX`
 plot(cpuMatrix$PRP,cpuMatrix$MMAX)
 
 #Grafica de scatter `para la correlacion de PRP y MMAX`
-plot(cpuDataMat$ERP,cpuDataMat$MMAX)
+plot(cpuMatrix$ERP,cpuMatrix$MMAX)
 
 #Grafica de scatter `para la correlacion de PRP y ERP`
-plot(cpuDataMat$ERP,cpuDataMat$PRP)
+plot(cpuMatrix$ERP,cpuMatrix$PRP)

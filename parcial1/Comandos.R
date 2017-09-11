8ecdfb2784a7683a889d8967305947e74e8fe3e6
Unknown <kevin_said@outlook.com>
2017-09-10 19:01:47 -0500

    Boxplot
    boxplots added

--- parcial1/Comandos.R
+++ parcial1/Comandos.R
@@ -20,9 +20,57 @@ plot(cpuMatrix$ERP,cpuMatrix$MMAX)
 
 #Grafica de scatter `para la correlacion de PRP y ERP`
 plot(cpuMatrix$ERP,cpuMatrix$PRP)
+<<<<<<< Updated upstream
 =======
 plot(cpuMatrix$ERP,cpuDataMat$MMAX)
 
 #Grafica de scatter `para la correlacion de PRP y ERP`
 plot(cpuMatrix$ERP,cpuDataMat$PRP)
+>>>>>>> Stashed changes
+=======
+
+#Boxplot of machine cycle time
+boxplot(cpuData$MYCT,
+        main = "Boxplot of Machine cycle time",
+        ylab = "Nanosenconds (Ns.)") 
+
+#Boxplot of the minimum main memory
+boxplot(cpuData$MMIN,
+        main = "Boxplot of Minimum main memory",
+        ylab = "Kilobytes (Kb.)")
+
+#Boxplot of the maximum main memory
+boxplot(cpuData$MMAX,
+        main = "Boxplot of Maximum main memory",
+        ylab = "Kilobytes (Kb.)") 
+
+#boxplot of the cache memory
+boxplot(cpuData$CACH,
+        main = "Boxplot of cache memory",
+        ylab = "Kilobytes (Kb.)") 
+
+#boxplot of the maximum main memory
+boxplot(cpuData$MMAX,
+        main = "Boxplot of Maximum main memory",
+        ylab = "Kilobytes (Kb.)")
+
+#boxplot of the minimum channels
+boxplot(cpuData$CHMIN,
+        main = "Boxplot of Minimum channels",
+        ylab = "Units (U.)")
+
+#boxplot of the maximum channels
+boxplot(cpuData$CHMAX,
+        main = "Boxplot of Maximum channels",
+        ylab = "Units (U.)")
+
+#boxploot of the relative performance
+boxplot(cpuData$PRP,
+        main = "Boxplot of published relative performance",
+        ylab = "Performance (P.)") 
+
+#boxplot of the estimated relative performance from original article
+boxplot(cpuData$MMAX,
+        main = "Boxplot of estimated relative performance from original article",
+        ylab = "Performance (P.)") 
 >>>>>>> Stashed changes

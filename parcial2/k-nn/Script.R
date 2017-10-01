b72f7698857f4e25efc4cff13159556b864e5cb4
Unknown <estebang056@gmail.com>
2017-09-30 20:41:53 -0500

    normalizacion

--- parcial2/k-nn/Script.R
+++ parcial2/k-nn/Script.R
@@ -1,5 +1,15 @@
 #Cargar el Dataset
 alumnos <-read.csv("dataset-alumnos-v2.csv")
+alumnos
 
 summary(alumnos)
+
+normalize = function(x){
+  return((x-min(x)) / (max(x)-min(x)))
+}
+alumnos[3:25]
+alumnos[alumnos=="NA"] <- 0
+alumnos = sapply(alumnos[3:25],normalize)
+
+alumnos
 

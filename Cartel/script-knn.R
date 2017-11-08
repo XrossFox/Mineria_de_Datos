06523b714ee7c7b933dfb076831757bfa0b6c784
XrossFox <warhead_1090@hotmail.com>
2017-11-08 11:16:33 -0600

    Ya funciona poke-knn

--- Cartel/script-knn.R
+++ Cartel/script-knn.R
@@ -10,18 +10,40 @@ poke_raw <- poke_raw[-10]
 poke_raw <- poke_raw[-8]
 poke_raw <- poke_raw[-13]
 poke_raw <- poke_raw[-5]
+poke_raw <- poke_raw[-13]
 
 # Funciona que normaliza
-normalize <- function(x){
-  return (x-min(x))/max(x)-min(x)
+normalize <- function(x) {
+  return ((x - min(x)) / (max(x) - min(x)))
 }
+# Convertir ints a numeric
+poke_raw <-as.data.frame(lapply(poke_raw,as.numeric))
 
 # Normalizar
 poke_norm <- as.data.frame(lapply(poke_raw,normalize))
+str(poke_norm$attack)
+
 
 #Ordenarlos al azar
-set.seed(504)
+set.seed(32165)
 poke_randomizer <- sample(800,640)
 
 poke_train <- poke_norm[poke_randomizer,]
 poke_test <- poke_norm[-poke_randomizer,]
+
+#Labels
+poke_train_labels <- as.factor(pokemon$is_legendary[poke_randomizer])
+poke_test_labels <- as.factor(pokemon$is_legendary[-poke_randomizer])
+
+round( prop.table(table(poke_train_labels))*100,digits=8)
+round( prop.table(table(poke_test_labels))*100,digits=8)
+
+# Aplicar knn
+library(class)
+pokemon_test_pred <- knn(train = poke_train,
+                         test = poke_test,
+                         cl = poke_train_labels,
+                         k = 1)
+
+# Tabla
+table(poke_test_labels,pokemon_test_pred)

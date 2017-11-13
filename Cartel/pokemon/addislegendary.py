7fe31b4192c90cf7577d8657d7b7623cd764c48e
Esteban García <estebang056@gmail.com>
2017-11-12 18:55:27 -0600

    Se creo el dataset legendarios contiene nombre pokedex y si es legendario

---
+++ Cartel/pokemon/addislegendary.py
@@ -0,0 +1,16 @@
+import csv
+#with open('pokemon.csv') as arc1:
+arc1 = open('pokemonentries.csv')
+csvfile = open('pokemon.csv')
+reader = csv.DictReader(csvfile)
+reader2 = csv.DictReader(arc1)
+i=1
+datasetknn = [row for row in reader]
+datasetpokedex = [row for row in reader2]
+print "name,pokedex,is_legendary"
+for row in datasetknn:
+    
+    for x in datasetpokedex:
+        if row['name'] == x['Nombre']:
+            print row['name']+','+x['Descripcion']+','+row['is_legendary']
+            
